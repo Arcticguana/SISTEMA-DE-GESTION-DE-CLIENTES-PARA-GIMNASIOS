a3c16658c8225d67c3458161a7b86f1440e1a179
Arcticguana <greenlantern0@hotmail.com>
2022-11-12 15:18:45 -0500

    Base de repositorio

---
+++ src/basededatos/ConexionBaseDatos.java
@@ -0,0 +1,9 @@
+package basededatos;
+
+public class ConexionBaseDatos {
+    private String cadenaConexion;
+
+    public ConexionBaseDatos(String cadenaConexion) {
+        this.cadenaConexion = cadenaConexion;
+    }
+}
